6ac65bdecd4722e296872f4bcf845ad9538a3f37
ErickMinax <erikminax@gmail.com>
2025-07-09 16:15:06 -0500

    Subida

--- src/config.js
+++ src/config.js
@@ -1,10 +1,9 @@
-// Ya sin IP, usa las rutas que definiste en vercel.json
 const API_BASE_URL = "/api";
 const API_ADMIN_BASE_URL = "/administrativos";
-const API_CITAS_BASE_URL = "/citas";
-const API_PAGOS_BASE_URL = "/pagos";
-const API_USUARIOS_BASE_URL = "/usuarios";
-const API_RECUPERAR_CLAVE_URL = "/recuperar-clave";
+const API_CITAS_BASE_URL = `${API_BASE_URL}/Citas`;
+const API_PAGOS_BASE_URL = `${API_BASE_URL}/Pagos`;
+const API_USUARIOS_BASE_URL = `${API_BASE_URL}/Usuarios`;
+const API_RECUPERAR_CLAVE_URL = `${API_USUARIOS_BASE_URL}/ObtenerNuevaClave`;
 
 export const API_NOTIFICACIONES = `${API_BASE_URL}/Notificaciones/ListarTodasNotificaciones`;
 
